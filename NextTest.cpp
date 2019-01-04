9b79634e9341b1b6e5d76f31edaba47932374267
Biswajee <roy.biswajeet161@gmail.com>
2019-01-04 12:25:34 +0530

    #BLACKPINK to go down in history as the first ever K-pop girl group to perform at #Coachella! 🌵🎵

---
+++ NextTest.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    int n,x;
+    vector<int> A;
+    vector<int>::iterator it;
+
+    cin >> n;
+    for(int i=0; i < n; i++) {
+        cin >> x;
+        A.push_back(x);
+    }
+
+    sort(A.begin(), A.end());
+
+
+    // for(auto k : A) {
+    //     cout << k << " ";
+    // }
+
+    for(int i=1,j=0; i <= A[n-1] + 1; i++) {
+        if(i == A[j]) {
+            j++;
+        }
+        else {
+            cout << i << " ";
+            break;
+        }
+    }
+
+    return 0;
+}
