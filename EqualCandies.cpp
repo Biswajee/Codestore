c87eaa6ef21cbd8ac1701a955efa597c43b08afd
Biswajee <roy.biswajeet161@gmail.com>
2019-01-18 00:32:32 +0530

    블핑하우스 😻🙆‍♂️

---
+++ EqualCandies.cpp
@@ -0,0 +1,24 @@
+/*
+
+You are distributing N candies to K children. You give 1 candy to the first child, 2 candies to the second child,
+3 candies to the third child and so on till K children have received the candies. You then start from the first child
+again and give him/her K+1 candies, K+2 candies to the second child and so on till all the candies are distributed.
+
+Find number of candies each child gets.
+
+Input Specifications:
+Input 1: Number of candies N.
+Input 2: Number of children K.
+
+Output Specifications:
+Return array A representing the candies all children get where A[i] is the number of candies received by ith child.
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#define blackpink ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+    return 0;
+}
