723d5f89a68161c3995210c5efa7f7125ab13a66
Biswajee <roy.biswajeet161@gmail.com>
2019-01-23 20:06:13 +0530

    Umm...messed up !

--- SmallerOnLeft.cpp
+++ SmallerOnLeft.cpp
@@ -1,38 +1,58 @@
+/*
+Given an array arr[] of N positive integers. The task is to find the greatest smaller on left of every element in the array.
+
+Input:
+First line of input contains number of testcases T. For each testcase, first line of input contains size of array N and next line contains array elements.
+
+Output:
+For each testcase, print the array which contains greatest smaller element on left of every corresponding element in the array. For the element whose smaller on left doesn't exists, consider -1 as greatest smaller.
+
+Constraints:
+1 <= T <= 10^0
+1 <= N <= 10^6
+1 <= arr[i] <= 10^8
+
+Example:
+Input:
+1
+5
+2 3 4 5 1
+
+Output:
+-1 2 3 4 -1
+
+Explanation:
+Testcase 1: Smallest element on left of 3 is 2, 4 is 3 and 5 is 1. Since 2 is the first element and no element on left is present, so it's greatest smaller element will be -1 and that for 1, no greatest smaller element is present on left, so it's greatest smaller element is -1.
+*/
+
+
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <set>
 #define BLACKPINK ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 
-
-int compute(vector<int> V, int n, int size) {
-    sort(V.begin(), V.begin()+ n);
-    return V[n-1];
-}
-
 int main() {
     BLACKPINK
-	int t,n,x,res;
-	cin >> t;
+	int t,n,x;
+    set<int> s;
+	// cin >> t;
+    t = 1;
 	
 	while(t--) {
-	    cin >> n;
-	    vector<int> A(n);
-	    for(int i=0; i<n; i++) {
-	        cin >> A[i];
-	    }
-	    for(int i=0; i,n; i++) {
-	        if((A[i] == *A.begin())) {
-	            cout << "-1 ";
-	        } else {
-	            if(A[i-1] > A[i]) {
-	                cout << "-1 ";
-	            } else {
-	                res = compute(A,i,n);
-	                cout << res << " ";
-	            } 
-	        }
-	    }
+        cin >> n;
+        s.insert(-1);
+        for(int i=0; i<n; i++) {
+            cin >> x;
+            
+            auto it = s.lower_bound(x);
+            cout << *it << " ";
+            s.insert(x);      
+        }
+        cout << endl;
+        for(int i: s) {
+            cout << i << " ";
+        }
 	}
 	return 0;
 }
