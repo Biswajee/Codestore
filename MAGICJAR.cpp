fc7c5249a8825bc39f7fa80c892a3ee7afa031ee
Biswajee <roy.biswajeet161@gmail.com>
2019-02-04 23:56:15 +0530

    츄의 심야방송 💖

---
+++ MAGICJAR.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define BLACKPINK ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+using namespace std;
+
+int main() {
+    BLACKPINK
+    ll t,n,minimum=0;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        for(ll i=0; i<n; i++) {
+            
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main() {
+//     BLACKPINK
+//     ll t,n,minimum=0; 
+//     cin >> t;
+//     while(t--) {
+//         cin >> n;
+//         ll a[n];
+//         for(ll i=0; i<n; i++) {
+//             cin >> a[i];
+//         }
+
+//         for(ll i=0; i<n; i++) {
+//             minimum += (a[i] - 1);
+//         }
+//         cout << minimum + 1 << endl;
+//     }
+//     return 0;
+// }
