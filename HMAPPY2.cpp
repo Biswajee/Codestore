607b9ac04722ab9361ebaf740a9332b693f026c7
Biswajee <roy.biswajeet161@gmail.com>
2019-02-04 22:48:25 +0530

    Finally done !

--- HMAPPY2.cpp
+++ HMAPPY2.cpp
@@ -1,51 +1,93 @@
 #include <bits/stdc++.h>
+#include <math.h>
 #define BLACKPINK ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
 using namespace std;
 
-bool isPrime(int n) 
-{ 
-    // Corner case 
-    if (n <= 1) 
-        return false; 
-    if(n == 2)
-        return false;
-  
-    // Check from 2 to n-1 
-    for (int i = 2; i < n; i++) 
-        if (n % i == 0) 
-            return false; 
-  
-    return true; 
-}
 
-
+// Fast Solution 
 int main() {
     BLACKPINK
-    long long int t,n,a,b,k;
+    ll t,n,a,b,k;
     cin >> t;
     while(t--) {
         cin >> n >> a >> b >> k;
-        long long int count = 0;
-        if((a > n) && (b > n)) {
-            count = 0;
-        }
-        else if(isPrime(a) == true && isPrime(b) == true) {
-            count = 2;
+        ll counta=0, countb=0, common=0, res=0;
+        if(a%b==0 || b%a == 0) {
+            counta = n/a;
+            countb = n/b;
+            res=abs(countb-counta);
         }
         else {
-            for(long long int i=1; i<=n; i++) {
-                if(i%a == 0 && i%b !=0) {
-                    count++;
-                }
-                if(i%b == 0 && i%a !=0) {
-                    count++;
-                }
-            }
+            counta = n/a;
+            countb = n/b;
+            common = n/(a*b);
+            res = counta + countb - (2*common);
         }
-        if(count >= k)
+        if(res >= k) 
         cout << "Win" << endl;
         else
         cout << "Lose" << endl;
     }
     return 0;
 }
+
+
+
+
+
+// #include <bits/stdc++.h>
+// #define BLACKPINK ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+// using namespace std;
+
+// bool isPrime(int n) 
+// { 
+//     // Corner case 
+//     if (n <= 1) 
+//         return false; 
+//     if(n == 2)
+//         return false;
+  
+//     // Check from 2 to n-1 
+//     for (int i = 2; i < n; i++) 
+//         if (n % i == 0) 
+//             return false; 
+  
+//     return true; 
+// }
+
+
+// int main() {
+//     BLACKPINK
+//     long long int t,n,a,b,k;
+//     cin >> t;
+//     while(t--) {
+//         cin >> n >> a >> b >> k;
+//         long long int count = 0, flag = 0;
+//         if((a > n) && (b > n)) {
+//             count = 0;
+//         }
+//         else if(isPrime(a) == true && isPrime(b) == true) {
+//             count = 2;
+//         }
+//         else {
+//             for(long long int i=1; i<=n; i++) {
+//                 if(i%a == 0 && i%b !=0) {
+//                     count++;
+//                 }
+//                 else if(i%b == 0 && i%a !=0) {
+//                     count++;
+//                 }
+//                  if(count >= k) {
+//                     cout << "Win" << endl;
+//                     flag = 1;
+//                     break;
+//                  }
+//             }
+//         }
+       
+//         if(flag == 0)
+//             cout << "Lose" << endl;
+//     }
+//     return 0;
+// }
