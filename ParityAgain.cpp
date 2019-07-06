4fc3f326ed6d81f962d27eafa38f6ed1add3abde
Biswajee <roy.biswajeet161@gmail.com>
2019-07-06 19:44:08 +0530

    In your Area ✔✔

---
+++ ParityAgain.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <set>
+#include <algorithm>
+#define ll long long int
+#define BLACKPINK ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+// calculate xor of numbers...
+ll cal_xor(ll x, ll y) { 
+   return (x | y) & (~x | ~y); 
+} 
+
+// number of 1's in bin rep...
+ll bin_one_count(ll n) {
+    string s = std::bitset<8>(n).to_string();
+    ll c = count(s.begin(), s.end(), '1');
+    return c;
+}
+
+int main() {
+	BLACKPINK
+	ll t, q, x, out;
+	cin >> t;
+	while(t--) {
+	    set<ll> s;
+	    vector<ll> v;
+	    cin >> q;
+	    while(q--) {
+	        cin >> x;
+	        s.insert(x);
+	        v.push_back(x);
+	        for(auto a : s) {
+	            if(a != x) {
+    	            out = cal_xor(a, x);
+    	            s.insert(out);
+	            }
+	        }
+	        ll E = 0, O = 0;
+	        for(auto a : s) {
+	           // cout << "array : " << a << endl;
+	            ll count = bin_one_count(a);
+	            if(count%2) {
+	                O++;
+	            } else {
+	                E++;
+	            }
+	        }
+	        cout << E << " " << O << endl;
+	    }
+	}
+	return 0;
+}
