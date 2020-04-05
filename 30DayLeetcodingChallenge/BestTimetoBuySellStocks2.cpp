7ebda2c94a26305e03fc773665332cdc8ea1346f
Biswajee <roy.biswajeet161@gmail.com>
2020-04-06 03:03:54 +0530

    @sooyaaa__ 꽃이 한가득💐

---
+++ 30DayLeetcodingChallenge/BestTimetoBuySellStocks2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int p = 0;
+        if(!prices.size()) return 0;
+        for(int i=0; i<prices.size()-1; i++)
+            if(prices[i] < prices[i+1]) p += prices[i+1]-prices[i]; // profit
+        return p;
+    }
+};
