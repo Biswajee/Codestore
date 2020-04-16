248be451ead0d057884cb77ed808029ff955c8de
Biswajee <roy.biswajeet161@gmail.com>
2020-04-16 05:55:06 +0530

    @roses_are_rosie More from Bali. We hope to be back Indonesia 🤍 Thank you for having us.

--- 30DayLeetcodingChallenge/ProductofanArrayExceptSelf.cpp
+++ 30DayLeetcodingChallenge/ProductofanArrayExceptSelf.cpp
@@ -26,3 +26,59 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res;
+        for(int i=0; i<nums.size(); i++) { // head
+            int prod = 1;
+            for(int j=0; j<nums.size(); j++) { // product pointer
+                if(i!=j) prod*=nums[j];
+            }
+            res.push_back(prod);
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res;
+        map<pair<int, int>, int> mp;
+        for(int i=0; i<nums.size(); i++) { // head
+            int prod = 1;
+            for(int j=0; j<nums.size(); j++) { // product pointer
+                if(i!=j) {
+                    if(!mp[{prod, nums[j]}]) prod*=nums[j];
+                    else prod*=mp[{prod, nums[j]}];
+                }
+            }
+            res.push_back(prod);
+        }
+        return res;
+    }
+};
+
+
+
+// Accepted
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res(nums.size());
+        for(int i=0; i<nums.size(); i++) res[i]=1;
+        int i=0, temp1=1, temp2=1;
+        for(; i<nums.size(); i++) {
+            res[i]*=temp1;
+            temp1*=nums[i];
+        }
+        i--;
+        for(; i>=0; i--) { 
+            res[i]*=temp2;
+            temp2*=nums[i];
+        }
+        return res;
+    }
+};
