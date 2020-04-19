b034b3080e336b0373bc7d9c58ea706c28d6880f
Biswajee <roy.biswajeet161@gmail.com>
2020-04-20 05:23:43 +0530

    @roses_are_rosie Taken by miss @lalalalisa_m. I think this one’s from Manchester �

--- 30DayLeetcodingChallenge/SearchinRotatedSortedArray.cpp
+++ 30DayLeetcodingChallenge/SearchinRotatedSortedArray.cpp
@@ -8,3 +8,31 @@ public:
         return -1;
     }
 };
+
+// Binary search solution...
+class Solution {
+public:
+    int bins(vector<int> v, int l, int r, int key) {
+        
+        while(l<=r) {
+            int mid=(l+r)/2;
+            // cout << mid << endl;
+            if(v[mid]==key) return mid;
+            else if(v[mid]>key) r=mid-1;
+            else if(v[mid]<key) l=mid+1;
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        int res;
+        if(nums.size()==0) return -1;
+        auto p = min_element(nums.begin(), nums.end());
+        int idx = p-nums.begin();
+        if(*p==target) return idx;
+        res = bins(nums, 0, idx-1, target);
+        if(res!=-1) return res;
+        res = bins(nums, idx, nums.size()-1, target);
+        return res;
+    }
+};
