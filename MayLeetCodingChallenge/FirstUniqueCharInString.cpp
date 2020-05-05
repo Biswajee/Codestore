b3298cc02001126e6eb86cac0d836e8c30b8a1fc
Biswajee <roy.biswajeet161@gmail.com>
2020-05-05 21:02:40 +0530

    @roses_are_rosie See u up there my loves 💚

---
+++ MayLeetCodingChallenge/FirstUniqueCharInString.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> mp;
+        for(char c : s) {
+            mp[c]++;
+        }
+        int i=0;
+        for(char c : s) {
+            if(mp[c]==1) return i;
+            i++;
+        }
+        return -1;
+    }
+};
