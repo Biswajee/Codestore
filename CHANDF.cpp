e8b74fbbf188b4b834b7f994478e49828757abfa
Biswajee <roy.biswajeet161@gmail.com>
2020-05-09 11:23:08 +0530

    @roses_are_rosie 🐾

---
+++ CHANDF.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <set>
+#include <algorithm>
+#define BLACKPINK ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+using namespace std;
+
+
+
+//  ll main() {
+//     BLACKPINK
+//     ull t,x,y,l,r;
+//     cin >> t;
+//     while(t--) {
+//         cin >> x >> y >> l >> r;
+//         ull mmax = ll_MIN;
+//         ull ans=1;
+//         for(ull i=l; i<=r; i++) {
+//             ull func = (x&i)*(y&i);
+//             if(func > mmax) { mmax = func; ans = i; }
+//         }
+//         cout << ans << endl;
+//     }
+//     return 0;
+// }
+
+
+ll countMSB(ll q, vector<ll> v) { 
+    ll inc = 0; 
+    for (ll i = 0; i < v.size(); i++) 
+        if ((q & v[i]) == q) 
+            inc++; 
+    return inc; 
+} 
+
+ll maxAND (ll xy, vector<ll> v) { 
+    ll ans = xy, c; 
+    for (ll bitpos = 62; bitpos >= 0; bitpos--) { 
+        c = countMSB(ans | (1 << bitpos), v); 
+        if ( c >= 2 ) ans |= (1 << bitpos);         
+    } 
+    return ans; 
+} 
+
+
+
+
+int main() {
+    BLACKPINK
+    ll t,x,y,l,r;
+    cin >> t;
+    while(t--) {
+        cin >> x >> y >> l >> r;
+
+        if(min(x,y)==0) {
+            cout << 0 << endl;
+            continue;
+        }
+        ll w = x|y;
+        if(w>=l && w <= r) {
+            cout << w << endl;
+            continue;
+        }
+        vector<ll> v;
+        for(ll i=l; i<=r; i++) v.push_back(i);
+        ll ans = maxAND(x&y, v);
+        cout << ans+1 << endl;
+    }
+    return 0;
+}
