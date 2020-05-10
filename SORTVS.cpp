531f16fb7852347700785f13ee2157c65e729433
Biswajee <roy.biswajeet161@gmail.com>
2020-05-10 18:56:58 +0530

    @roses_are_rosie it takes two

--- SORTVS.cpp
+++ SORTVS.cpp
@@ -5,18 +5,16 @@
 #define ll long long
 using namespace std;
 
-set<pair<ll,ll>> actual_trans1;
-set<pair<ll,ll>> actual_trans2;
-void inversion_count(vector<ll> v) {
+
+ll inversion_count(vector<ll> v) {
     ll inv = 0; 
     ll n = v.size();
     for (ll i = 0; i < n - 1; i++) 
         for (ll j = i + 1; j < n; j++) 
-            if (v[i] > v[j]) {
-                actual_trans1.insert({v[i], v[j]});
-                actual_trans2.insert({v[j], v[i]});
+            if (v[i] > v[j]) 
                 inv++; 
-            } 
+  
+    return inv;
 }
 
 
@@ -57,30 +55,25 @@ int main() {
     while(t--) {
         cin >> n >> m;
         vector<ll> v(n);
-        set<pair<ll,ll>> transformations;
-        actual_trans1.clear();
-        actual_trans2.clear();
         for(ll i=0; i<n; i++) cin >> v[i];
         for(ll i=0; i<m; i++) {
             cin >> x >> y;
-            transformations.insert({y,x});
-            swap(v[x-1], v[y-1]);
+            if(v[x-1] < v[y-1]) swap(v[x-1], v[y-1]);
         }
-        
-        // cout << "pre swap array status..." << endl;
+        // vector<pair<ll,ll>> idx_arranged(n);
         // for(ll i=0; i<n; i++) {
-        //     cout << v[i] << " ";
+        //     idx_arranged[i].first = i;
+        //     idx_arranged[i].second = v[i];
         // }
-        // cout << endl;
-        inversion_count(v);
-        // cout << "\nActual inversions :" << endl;
-        // for(auto x : actual_trans) cout << x.first << "," << x.second << endl;
-        // cout << "\nGiven inversions :" << endl;
-        // for(auto x : transformations) cout << x.first << "," << x.second << endl;
 
-        bool res1 = includes(transformations.begin(), transformations.end(), actual_trans1.begin(), actual_trans1.end());
-        bool res2 = includes(transformations.begin(), transformations.end(), actual_trans2.begin(), actual_trans2.end());
-        if(actual_trans1.size()!=0 && actual_trans2.size()!=0 && (res1 || res2)) cout << 0 << endl;
+        // for(ll i=0; i<n; i++) {
+        //     cout << "{ " << idx_arranged[i].first << ", " << idx_arranged[i].second  << " }" << endl;
+        // }
+
+        if(inversion_count(v) == 0) {
+            // Array is sorted...
+            cout << 0 << endl;
+        }
         else {
             ll minutes = min_swaps(v);
             cout << minutes << endl;
