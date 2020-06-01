d8fcc2305ab684ca1e6cce608ed4c3fbcceae621
Biswajee <roy.biswajeet161@gmail.com>
2020-06-01 20:37:35 +0530

    @roses_are_rosie 방예담 솔로데뷔 화이팅git status 노래 너무 좋아요git status 예담이 많이 응원해주세요!

---
+++ BinaryMirrorTree.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//function Template for C++
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+/* Should convert tree to its mirror */
+void mirror(Node* node) 
+{
+     if(node == NULL) return;
+     else {
+         mirror(node->left);
+         mirror(node->right);
+         
+         Node* temp = node->left;
+         node->left = node->right;
+         node->right = temp;
+     }
+}
