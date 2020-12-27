a3f059185dde9e34bc4091991f8f217dec897dc8
Biswajee <roy.biswajeet161@gmail.com>
2020-12-27 19:33:52 +0530

    Update O(n) solution

--- ContainerWithMostWater.cpp
+++ ContainerWithMostWater.cpp
@@ -22,3 +22,36 @@ int main() {
 	}
 	return 0;
 }
+
+
+// O(n)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0, j=height.size()-1;
+        int mx=0;
+        while(i<j) {
+            int mn = min(height[i], height[j]);
+            int area = mn * abs(j-i);
+            mx = max(mx, area);
+            if(mn == height[i]) i++;
+            if(mn == height[j]) j--;
+        }
+        return mx;
+    }
+};
+
+// O(n^2)
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int mx = 0;
+//         for(int i=0; i<height.size(); i++) {
+//             for(int j=i; j<height.size(); j++) {
+//                 int area = min(height[i], height[j]) * abs(j-i);
+//                 mx = max(mx, area);
+//             }
+//         }
+//         return mx;
+//     }
+// };
