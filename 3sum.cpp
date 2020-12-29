0dc568fb5967dab019ea154e430758202843cd9a
Biswajee <roy.biswajeet161@gmail.com>
2020-12-29 19:38:35 +0530

    @roses_are_rosie ❄️

---
+++ 3sum.cpp
@@ -0,0 +1,36 @@
+// O(n^2) Solution
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        vector<vector<int>>res;
+        int n = nums.size();
+        int sum = 0, i, j, k, target;
+        if(n == 0) return res;
+        
+        unordered_map<string, bool> unique_targets;
+        
+        sort(nums.begin(), nums.end());
+        
+        for(i=0; i<n-2; i++) {
+            target = 0-nums[i];
+            j = i+1, k = n-1, sum = 0;
+            while(j<k) {
+                sum = nums[j] + nums[k];
+
+                if(sum == target) { 
+                    string buffer = to_string(nums[i])+to_string(nums[j])+to_string(nums[k]);
+                    if(!unique_targets[buffer]) {
+                        res.push_back({nums[i],nums[j],nums[k]});
+                        unique_targets[buffer] = true;
+                    }
+                }
+                // how close result is to target value...
+                if(sum > target) k--;
+                if(sum <= target) j++;
+            }
+        }
+        
+        return res;
+    }
+};
