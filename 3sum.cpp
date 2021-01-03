1d25fb0360475d216b7b75399447993905dd9366
Biswajee <roy.biswajeet161@gmail.com>
2021-01-03 13:51:48 +0530

    Jugaad code

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,3 +1,39 @@
+// O(n^2) but with jugaad
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        set<vector<int>> st;
+        int n = nums.size();
+        int sum = 0, i, j, k, target;
+        if(n == 0) return {};
+        
+        sort(nums.begin(), nums.end());
+        unordered_map<string, bool> unique_targets;
+        
+        for(i=0; i<n-2; i++) {
+            target = 0-nums[i];
+            j = i+1, k = n-1, sum = 0;
+            if(i==0 || nums[i]!=nums[i-1]) {
+                while(j<k) {
+                    sum = nums[j] + nums[k];
+                    // how close result is to target value...
+                    if(sum > target) k--;
+                    else if(sum < target) j++;
+
+                    else {
+                        st.insert({nums[i],nums[j],nums[k]});
+                        j++; 
+                        k--;
+                    }
+                }
+            }
+        }
+        vector<vector<int>>res(st.begin(), st.end());
+        return res;
+    }
+};
+
 // O(n^2) Solution
 class Solution {
 public:
